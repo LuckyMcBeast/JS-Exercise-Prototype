f96feb3d164e17e472b77a6b46c196d47d75204a
M. Cullen McClellan <cullenmcclellan@gmail.com>
2020-09-08 22:53:48 -0400

    Minor readablility adjustment

--- index.js
+++ index.js
@@ -103,9 +103,9 @@ Car.prototype.fill = function(gallons){
 
 Car.prototype.drive = function(distance){
   if(this.tank-distance/this.milesPerGallon <= 0){
-  this.odometer = this.tank*this.milesPerGallon;
-  this.tank = 0;
-  return `I ran out of fuel at ${this.odometer} miles!`
+    this.odometer = this.tank*this.milesPerGallon;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`
   }
   this.odometer += distance;
   this.tank -= distance/this.milesPerGallon;
